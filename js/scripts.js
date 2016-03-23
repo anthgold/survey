d393912307ed3e65cd5e13996a327e9a1c5fe51a
Anthony Gold and Kevin Deganos <kevin@>
2016-03-23 15:07:33 -0700

    created project template

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $(".jumbotron form").submit(function(event) {
+
+    var nameInput = $("input#name").val();
+    var dateInput = $("input#date").val();
+    var startInput = $("input#start").val();
+    var endInput = $("input#end").val();
+
+    $(".name").text(nameInput);
+    $(".date").text(dateInput);
+    $(".start").text(startInput);
+    $(".end").text(endInput);
+
+    $("#confirmation").show();
+
+    event.preventDefault();
+  });
+});
